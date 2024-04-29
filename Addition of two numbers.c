08becb3ed3e91fd99b692cac0ce7cdb6b62e214b
Tumit Dass <146959253+Tes-miT@users.noreply.github.com>
2024-04-29 20:03:09 +0530

    Create Addition of two numbers.c
    Added 'Addition of two numbers' being the first problem solving program.

---
+++ Addition of two numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+        int first, second;
+        printf("Please enter 2 numbers : ");
+        scanf("%d%d", &first, &second);
+
+        int sum = first + second;
+        printf("%d", sum);
+
+        return 0;
+}
